6202511653060801ef4366658cb7cdc921f85a2b
moisas <mopri08@gmail.com>
2026-09-09 14:02:37 -0300

    criado variaveis para centralização da quantidade de caracteres e utilização da função de validação atualizada

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,27 +1,28 @@
 from pydantic import BaseModel, EmailStr, Field, field_validator
 from datetime import datetime
-from typing import Optional
+from typing import Optional, ClassVar
 from app.core.validators import clean_and_validate_string
 
 class UserCreate(BaseModel):
-    name: str = Field(..., min_length=3, description='O nome deve ter pelo menos, 3 caracteres!')
+    min_length_name: ClassVar[int] = 3
+    min_length_password: ClassVar[int] = 6
+
+    name: str = Field(..., min_length=min_length_name, description=f'O nome deve ter pelo menos, {min_length_name} caracteres!')
+    email: EmailStr
+    password: str = Field(..., min_length=min_length_password, description=f'A senha deve ter no minimo, {min_length_password} caracteres!')
+    abre_rnc: Optional[bool] = False
+    responsavel_setor: Optional[bool] = False
     
     @field_validator('name')
     @classmethod
     def validate_name(cls, v: str) -> str:
-        cleaned = clean_and_validate_string(v)
+        cleaned = clean_and_validate_string(v, cls.min_length_name)
         return cleaned.upper()
     
-    email: EmailStr
-    
-    password: str = Field(..., min_length=6, description='A senha deve ter no minimo, 6 caracteres!')
     @field_validator('password')
     @classmethod
     def validate_password(cls, v: str) -> str:
-        return clean_and_validate_string(v)
-        
-    abre_rnc: Optional[bool] = False
-    responsavel_setor: Optional[bool] = False
+        return clean_and_validate_string(v, cls.min_length_password)
 
 class UserResponse(BaseModel):
     id: int
@@ -31,10 +32,12 @@ class UserResponse(BaseModel):
     responsavel_setor: bool
     created_at: datetime
 
-    class Config:
-        from_attributes = True
+    model_config = {'from_attributes': True}
         
 class UserUpdate(BaseModel):
+    min_length_name: ClassVar[int] = 3
+    min_length_password: ClassVar[int] = 6
+
     name: Optional[str] = None
     email: Optional[EmailStr] = None
     password: Optional[str] = None
@@ -46,19 +49,12 @@ class UserUpdate(BaseModel):
     def updated_name(cls, v: Optional[str]) -> Optional[str]:
         if v is None:
             return None
-        
-        cleaned = clean_and_validate_string(v)
-        if len(cleaned) < 3:
-            raise ValueError('O nome deve ter pelo menos, 3 caracteres!')
+        cleaned = clean_and_validate_string(v, cls.min_length_name)
         return cleaned.upper()
     
     @field_validator('password')
     @classmethod
-    def update_password(cls, v:Optional[str]) -> Optional[str]:
+    def update_password(cls, v: Optional[str]) -> Optional[str]:
         if v is None:
             return None
-        
-        cleaned = clean_and_validate_string(v)
-        if len(cleaned) < 6:
-            raise ValueError('A senha deve ter no minimo, 6 caracteres!')
-        return cleaned
+        return clean_and_validate_string(v, cls.min_length_password)
